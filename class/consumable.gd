64baa9357943e66f4b9b7bdc99ef7d9ab4e18ce4
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2022-08-04 16:15:26 +0200

    Update consumable.gd

--- class/consumable.gd
+++ class/consumable.gd
@@ -1,3 +1,9 @@
 class_name Consumable
 extends Item
 
+var combine_recipes:Array 
+# Multiple recipes possible
+# Material A, Material B
+var shops:Array
+# Multiple shops possible
+# Shop Name, Price
