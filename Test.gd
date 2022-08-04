5a156e65750f9a215e46d81d8b34a526a83d32ba
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2022-08-04 16:15:10 +0200

    Rename WeaponTest to Test

---
+++ Test.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+func _ready() -> void:
+	
+	
+#	var ironSword = BlademasterWeapon.new(Item.Type.WEAPON, 1, 100, 675, "Iron Sword+", Weapon.Style.GREAT_SWORD,
+#	 Weapon.ElementStatus.NONE, 336, 0, BlademasterWeapon.Sharpness.YELLOW)
+#	var crossBowGun = GunnerWeapon.new(Item.Type.WEAPON, 1, 100, 450, "Crossbow Gun", Weapon.Style.LIGHT_BOWGUN,
+#	Weapon.ElementStatus.NONE, 96, 0, GunnerWeapon.Reload.NORMAL,
+#	GunnerWeapon.Recoil.MODERATE)
+#
+#	print(ironSword.getStats())
+#	print(crossBowGun.getStats())
