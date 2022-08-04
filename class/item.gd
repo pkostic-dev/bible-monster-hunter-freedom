05d86297e656ebefd69ec85196321bfdd6413134
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2022-08-04 16:15:40 +0200

    Update item.gd
    Remove export

--- class/item.gd
+++ class/item.gd
@@ -1,16 +1,20 @@
 class_name Item
 
+
 enum Type {CONSUMABLE, GATHERING, HUNTING, SUPPLY, ACCOUNT, AMMO, PLANT,
 MUSHROOM, SEED, MINERAL, FISH, INSECT, SAC, BONE, MATERIAL, UNIQUE, BOOK,
 TREASURE, WEAPON, ARMOR}
 
-export(Type) var type
-export(int) var rarity
-export(int) var capacity
-export(int) var value # sell value in zenny
-# info
 
-func _init(_type: int, _rarity: int = 1, _capacity: int = 1, _value: int = 0) -> void:
+var icon:String
+var description:String
+var type:int
+var rarity:int
+var capacity:int
+var value:int # Sell value in zenny
+
+
+func _init(_icon: String, _type: int, _rarity: int = 1, _capacity: int = 1, _value: int = 0) -> void:
 	type = _type
 	rarity = _rarity
 	capacity = _capacity
