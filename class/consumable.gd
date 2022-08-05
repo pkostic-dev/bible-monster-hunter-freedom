b4cb11152ac1cbe2de9763f0502fe6d739d78686
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2022-08-05 04:36:56 +0200

    Update consumable.gd

--- class/consumable.gd
+++ class/consumable.gd
@@ -7,3 +7,11 @@ var combine_recipes:Array
 var shops:Array
 # Multiple shops possible
 # Shop Name, Price
+
+func _init(
+_name: String = "", _type: int = Item.Type.CONSUMABLE, _icon: String = "",_description: String = "",
+_rarity: int = 1, _capacity: int = 1, _value: int = 0,
+_combine_recipes: Array = [], _shops: Array = []).(_name, _type, _icon,
+_description, _rarity, _capacity, _value) -> void:
+	combine_recipes = _combine_recipes
+	shops = _shops
