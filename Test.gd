d29d750ea79c30eb8418c2e02344d308724498aa
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2022-08-22 18:00:29 +0200

    Update Test.gd

--- Test.gd
+++ Test.gd
@@ -2,19 +2,25 @@ extends Node
 
 
 func _ready() -> void:
-	var combinable_items = load_from_json("res://data/combinable_items.json", CombinableItem)
+	var combinable_items = combinable_items_from_json("res://data/combinable_items.json")
 	print(combinable_items)
 
-func load_from_json(path, object_type) -> Array:
+
+func file_from_json(path) -> Dictionary:
 	var file = File.new()
 	if not file.file_exists(path):
 		printerr("File doesn't exist at " + path)
-		return []
+		return {}
 	file.open(path, File.READ)
 	var data = parse_json(file.get_as_text())
 	if data.empty():
 		printerr("File is empty")
-		return []
+		return {}
+	return data
+
+
+func combinable_items_from_json(path) -> Array:
+	var data = file_from_json(path)
 	
 	var type = data.keys()[0]
 	var items = data.get(type)
@@ -26,7 +32,7 @@ func load_from_json(path, object_type) -> Array:
 		var _type = Item.Type.get(type.to_upper())
 		var _name = item
 		var _icon = item_dict.get("icon")
-		var object = object_type.new(_name, _type, _icon)
+		var object = CombinableItem.new(_name, _type, _icon)
 		items_list.append(object)
 		
 	return items_list
