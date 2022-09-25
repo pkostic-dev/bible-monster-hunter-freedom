2be20e226e5535976bb327fdb4885cc1b59d6515
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2022-09-25 21:41:36 +0200

    Create quest.gd

---
+++ class/quest.gd
@@ -0,0 +1,22 @@
+class_name Quest
+
+enum Type {VILLAGE, GUILD}
+enum EnvironmentType {STABLE, UNSTABLE}
+
+var type:int
+var stars:int
+var number:int
+var name:String
+var objective:String
+var location:String
+var reward:int
+var contract_fee:int
+var time_limit:int
+var environment:int
+var hr_points:int
+var requirement:String
+var monsters:Array[String]
+var requestor:String
+var description:String
+var cutscenes:Array[String] # Area 
+var boulders:Array[Array] # (area, (to) area)
