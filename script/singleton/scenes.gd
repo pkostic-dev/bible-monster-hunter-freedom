83035a0833f89cec0eee3b3ab226e8db6685876f
pkostic-dev <23319698+pkostic-dev@users.noreply.github.com>
2022-10-13 18:01:41 +0200

    Create scenes.gd

---
+++ script/singleton/scenes.gd
@@ -0,0 +1,15 @@
+extends Node
+
+var MenuScene := "res://scene/Menu.tscn"
+
+var WeaponsScene := "res://scene/Weapons.tscn"
+var ItemsScene := "res://scene/Items.tscn"
+var ArmorsScene := "res://scene/Armor.tscn"
+
+var MonstersScene := "res://scene/Monsters.tscn"
+var QuestsScene := "res://scene/Quests.tscn"
+var CombinationsScene := "res://scene/Combinations.tscn"
+
+var LocationsScene := "res://scene/Locations.tscn"
+var FarmScene := "res://scene/Farm.tscn"
+var KitchenScene := "res://scene/Kitchen.tscn"
