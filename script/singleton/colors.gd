efbdc1847eb56840a97b756b0b317e51805701af
Predrag Kostic <23319698+pkostic-dev@users.noreply.github.com>
2023-10-04 12:21:43 +0200

    Update colors.gd

--- script/singleton/colors.gd
+++ script/singleton/colors.gd
@@ -1,5 +1,7 @@
 extends Node
 
 
-var green := Color(0.43, 0.78, 0.51)
-var pink := Color(0.91, 0.56, 0.62)
+var green := Color(0.36, 0.68, 0.44)
+var pink := Color(0.76, 0.47, 0.52)
+var blue := Color(0.37, 0.42, 0.82)
+var orange := Color(0.82, 0.50, 0.29)
